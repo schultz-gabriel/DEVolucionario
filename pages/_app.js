757c3c772112aa34d222490da02ca957d5cab381
Carol <carolandrades11@gmail.com>
2021-08-15 21:28:21 -0300

    build: arquivo inicial (ESLint, Husky, propTypes e Conventional Commits)
    Arquivo inicial Next.js com styled-components. Pacotes já adicionados: ESLint, Husky, propTypes e
    Conventional Commits. Todos já configurados.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -6,21 +8,29 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-`
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Component
+        // eslint-disable-next-line react/jsx-props-no-spreading
+          {...pageProps}
+        />
       </ThemeProvider>
     </>
-  )
+  );
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.shape({}).isRequired,
+};
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title>My page</Title>;
 }
