03aeaa6493cae82fa64c4df729f9ef303977c679
Gabriel Schultz <gabriel.bridi2008@hotmail.com>
2021-11-17 21:02:46 -0300

    feat(pages): creating post page

---
+++ pages/blog/[slug].jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import websitePageHOC from '../../src/components/wrappers/WebsitePage/hoc';
+import PostScreen from '../../src/screens/PostScreen';
+
+import posts from '../../blog_posts';
+
+function PostPage({ post }) {
+  return (
+    <PostScreen post={post} />
+  );
+}
+
+PostPage.propTypes = PostScreen.propTypes;
+
+export default websitePageHOC(PostPage);
+
+export async function getStaticProps({ params }) {
+  const pageData = posts.reduce((accumulatedValues, post) => {
+    const foundPost = post.slug === params.slug;
+
+    if (foundPost) {
+      return {
+        accumulatedValues,
+        postData: post,
+      };
+    }
+    return accumulatedValues;
+  }, {});
+  return {
+    props: {
+      post: pageData.postData,
+      pageWrapperProps: {
+        seoProps: {
+          headTitle: pageData.postData.title,
+        },
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = posts.reduce((accumulatedValues, post) => {
+    const projectSlug = post.slug;
+    return [
+      ...accumulatedValues,
+      { params: { slug: projectSlug } },
+    ];
+  }, []);
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
