a4b75ed0b38a46a9db2148bbd1d33170ac2fa0f6
Gabriel Schultz <gabriel.bridi2008@hotmail.com>
2021-12-29 14:44:11 -0300

    feat(cookies): theme cookie setted

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,40 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { parseCookies, setCookie } from 'nookies';
 import theme from '../src/theme';
 import { dark, light } from '../src/theme/colorThemes';
 import GlobalStyle from '../src/theme/GlobalStyle';
 
 export default function App({ Component, pageProps }) {
   const [colorTheme, setColorTheme] = useState(dark);
-  const toggleTheme = () => {
-    setColorTheme(colorTheme.title === 'light' ? dark : light);
+
+  const toggleTheme = (selectedTheme) => {
+    setColorTheme(selectedTheme);
   };
+
+  const getThemeCookie = () => {
+    const cookies = parseCookies();
+    const themeCookie = cookies.THEME_COOKIE;
+    setColorTheme(themeCookie === 'dark' ? dark : light);
+  };
+
+  const setThemeCookie = (selectedTheme) => {
+    const setCookieModule = setCookie;
+    const DAY_IN_SECONDS = 86400;
+    setCookieModule(null, 'THEME_COOKIE', selectedTheme, {
+      maxAge: DAY_IN_SECONDS * 7,
+    });
+  };
+
+  const handleToggleTheme = () => {
+    toggleTheme(colorTheme.title === 'light' ? dark : light);
+    setThemeCookie(colorTheme.title === 'light' ? 'dark' : 'light');
+  };
+
+  useEffect(() => {
+    getThemeCookie();
+  }, []);
 
   const allThemes = {
     ...theme,
@@ -20,7 +45,7 @@ export default function App({ Component, pageProps }) {
     <>
       <ThemeProvider theme={allThemes}>
         <GlobalStyle />
-        <Component toggleTheme={toggleTheme} {...pageProps} />
+        <Component toggleTheme={handleToggleTheme} {...pageProps} />
       </ThemeProvider>
     </>
   );
